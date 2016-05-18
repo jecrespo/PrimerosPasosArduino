49f546c9dafe67e34ba9d0df30a4680add36a483
Enrique Crespo <ecrespo@arsys.es>
2016-05-19 00:21:17 +0200

    Simulando la lectura de un relé, sacar por puerto serie **ALARMA** cuando ponga GND el pin 7

--- PrimerosPasosArduino.ino
+++ PrimerosPasosArduino.ino
@@ -2,14 +2,17 @@ void setup() {
   // put your setup code here, to run once:
   pinMode(13, OUTPUT);
   pinMode(7, INPUT_PULLUP);
+  Serial.begin(9600);
 }
 void loop() {
   // put your main code here, to run repeatedly:
   int lectura = digitalRead(7);
   if (lectura == 0) {
     digitalWrite(13, HIGH);
+    Serial.println("ALARMA!!!!!!");
   }
   else {
     digitalWrite(13, LOW);
+    Serial.println("NO HAY ALARMA");
   }
 }
