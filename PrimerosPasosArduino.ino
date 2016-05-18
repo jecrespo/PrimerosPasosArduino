d1f232639d6bfe5daa92efaeac9b25b7ab43b733
Enrique Crespo <ecrespo@arsys.es>
2016-05-19 00:25:08 +0200

    Añadir una variable con el estado de alarma y compararlo el estado anterior para madar **ALARMA** o **RECUPERACION ALARMA**

--- PrimerosPasosArduino.ino
+++ PrimerosPasosArduino.ino
@@ -1,18 +1,27 @@
+boolean estado_alarma = 0;
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(13, OUTPUT);
   pinMode(7, INPUT_PULLUP);
   Serial.begin(9600);
 }
+
 void loop() {
   // put your main code here, to run repeatedly:
   int lectura = digitalRead(7);
   if (lectura == 0) {
-    digitalWrite(13, HIGH);
-    Serial.println("ALARMA!!!!!!");
+    if (estado_alarma == 0) {
+      Serial.println("ALARMA!!!!!!");
+      digitalWrite(13, HIGH);
+      estado_alarma = 1;
+    }
   }
   else {
-    digitalWrite(13, LOW);
-    Serial.println("NO HAY ALARMA");
+    if (estado_alarma == 1) {
+      digitalWrite(13, LOW);
+      estado_alarma = 0;
+      Serial.println("ALARMA RECUPERADA");
+    }
   }
 }
